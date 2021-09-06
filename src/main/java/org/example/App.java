0c1ec56402403cd23450de33f4cbd1400b2efc67
Reese Stowell <Reese2as@gmail.com>
2021-09-05 21:01:21 -0400

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+/*
+UCF COP3330 Fall 2021 Exercise 1 Solution
+Copyright 2021 Reese Stowell
+*/
+package org.example;
+import java.util.Scanner;
+
+public class App {
+    public static void main(String[] args)
+    {
+        Scanner user_input = new Scanner(System.in);
+        String input;
+        System.out.print("What is the input string?");
+        input = user_input.next();
+        int stringLength = input.length();
+        System.out.println(String.format("%s has %d characters",input,stringLength));
+    }
+}
